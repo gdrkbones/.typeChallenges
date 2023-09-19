d9b72db5d115f20c2abc266282873dadf785615d
julio.santiesteban@avangenio.com <julio.santiesteban@avangenio.com>
2023-09-18 22:33:56 -0400

    solve 5

--- 00005-extreme-readonly-keys.ts
+++ 00005-extreme-readonly-keys.ts
@@ -1,23 +1,34 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
 type cases = [
-  Expect<Equal<'title', GetReadonlyKeys<Todo1>>>,
-  Expect<Equal<'title' | 'description', GetReadonlyKeys<Todo2>>>,
-]
+	Expect<Equal<"title", GetReadonlyKeys<Todo1>>>,
+	Expect<Equal<"title" | "description", GetReadonlyKeys<Todo2>>>
+];
 
 interface Todo1 {
-  readonly title: string
-  description: string
-  completed: boolean
+	readonly title: string;
+	description: string;
+	completed: boolean;
 }
 
 interface Todo2 {
-  readonly title: string
-  readonly description: string
-  completed?: boolean
+	readonly title: string;
+	readonly description: string;
+	completed?: boolean;
 }
 
-
 // ============= Your Code Here =============
-type GetReadonlyKeys<T> = any
+
+type MyReadonly<T> = { readonly [K in keyof T]: T[K] };
+
+type EqualByParam<T, K extends keyof T> = Equal<
+	Pick<T, K>,
+	MyReadonly<Pick<T, K>>
+>;
+
+type GetReadonlyKeys<T> = keyof {
+	[K in keyof T as EqualByParam<T, K> extends true ? K : never]: T[K];
+};
+
+type test = GetReadonlyKeys<Todo2>;
